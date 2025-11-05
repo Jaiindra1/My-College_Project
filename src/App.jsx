7a61e54410bc7f6b1324b00fa8ddf6afba4274c5
Jai indra <168549352+Jaiindra1@users.noreply.github.com>
2025-11-05 20:16:02 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { AuthProvider } from "./context/AuthContext";
 import PrivateRoute from "./components/PrivateRoute";
-
+import ForgotPassword from "./pages/ForgotPassword";
 import Login from "./pages/Login";
 import AdminRoutes from "./routes/AdminRoutes";
 import FacultyRoutes from "./routes/FacultyRoutes";
 import StudentRoutes from "./routes/StudentRoutes";
+import ResetPassword from "./pages/ResetPassword";
 
 function App() {
   return (
@@ -37,6 +38,8 @@ function App() {
               </PrivateRoute>
             }
           />
+          <Route path="/forgot-password" element={<ForgotPassword />} />
+        <Route path="/reset-password" element={<ResetPassword />} />
         </Routes>
       </Router>
     </AuthProvider>
